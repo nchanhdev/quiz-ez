57fe2b0052300edda22b9366b9c14fff23df4401
Chanh Nguyen <chanhnguyen@Chanhs-MacBook-Pro.local>
2024-12-02 23:34:03 -0500

    feat-1

--- script.js
+++ script.js
@@ -1,111 +1,60 @@
+let quizData = [];
+
 fetch("data.json")
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => {
+    console.log(json);
+    
+    quizData = json
+  });
 
-let Questions = [];
-const ques = document.getElementById("ques");
+const questionElement = document.getElementById("question");
+const optionsElement = document.getElementById("options");
+const submitButton = document.getElementById("submit");
 
-async function fetchQuestions() {
-  try {
-    const response = await fetch("https://opentdb.com/api.php?amount=10");
-    if (!response.ok) {
-      throw new Error(`Something went wrong!!
-        Unable to fecth the data`);
-    }
-    const data = await response.json();
-    Questions = data.results;
-  } catch (error) {
-    console.log(error);
-    ques.innerHTML = `<h5 style='color: red'>
-        ${error}</h5>`;
-  }
-}
-fetchQuestions();
-
-let currQuestion = 0;
+let currentQuestion = 0;
 let score = 0;
 
-if (Questions.length === 0) {
-  ques.innerHTML = `<h5>Please Wait!! 
-    Loading Questions...</h5>`;
+function showQuestion() {
+  console.log(quizData);
+  
+  const question = quizData[currentQuestion];
+  questionElement.innerText = question.question;
+
+  optionsElement.innerHTML = "";
+  question.options.forEach(option => {
+    const button = document.createElement("button");
+    button.innerText = option;
+    optionsElement.appendChild(button);
+    button.addEventListener("click", selectAnswer);
+  });
 }
 
-function loadQues() {
-  const opt = document.getElementById("opt");
-  let currentQuestion = Questions[currQuestion].question;
-  if (currentQuestion.indexOf("&quot;") > -1) {
-    currentQuestion = currentQuestion.replace(/&quot;/g, '"');
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const answer = quizData[currentQuestion].answer;
+
+  if (selectedButton.innerText === answer) {
+    score++;
   }
-  if (currentQuestion.indexOf("") > -1) {
-    currentQuestion = currentQuestion.replace(/'/g, "'");
+
+  currentQuestion++;
+
+  if (currentQuestion < quizData.length) {
+    showQuestion();
+  } else {
+    showResult();
   }
-  ques.innerText = currentQuestion;
-  opt.innerHTML = "";
-  const correctAnswer = Questions[currQuestion].correct_answer;
-  console.log(Questions);
-  const incorrectAnswers = Questions[currQuestion].incorrect_answers;
-  const options = [correctAnswer, ...incorrectAnswers];
-  options.sort(() => Math.random() - 0.5);
-  options.forEach((option) => {
-    if (option.indexOf("&quot;") > -1) {
-      option = option.replace(/&quot;/g, '"');
-    }
-    if (option.indexOf("") > -1) {
-      option = option.replace(/'/g, "'");
-    }
-    const choicesdiv = document.createElement("div");
-    const choice = document.createElement("input");
-    const choiceLabel = document.createElement("label");
-    choice.type = "radio";
-    choice.name = "answer";
-    choice.value = option;
-    choiceLabel.textContent = option;
-    choicesdiv.appendChild(choice);
-    choicesdiv.appendChild(choiceLabel);
-    opt.appendChild(choicesdiv);
-  });
+}
+
+function showResult() {
+  quiz.innerHTML = `
+    <h1>Quiz Completed!</h1>
+    <p>Your score: ${score}/${quizData.length}</p>
+  `;
 }
 
 setTimeout(() => {
-  loadQues();
-  if (Questions.length === 0) {
-    ques.innerHTML = `<h5 style='color: red'>Unable 
-        to fetch data, Please try again!!</h5>`;
-  }
-}, 2000);
+  showQuestion();
+}, 100);
 
-function loadScore() {
-  const totalScore = document.getElementById("score");
-  totalScore.textContent = `You scored ${score} out 
-    of ${Questions.length}`;
-  totalScore.innerHTML += "<h3>All Answers</h3>";
-  Questions.forEach((el, index) => {
-    totalScore.innerHTML += `<p>${index + 1}.
-         ${el.correct_answer}</p>`;
-  });
-}
-
-function nextQuestion() {
-  if (currQuestion < Questions.length - 1) {
-    currQuestion++;
-    loadQues();
-  } else {
-    document.getElementById("opt").remove();
-    document.getElementById("ques").remove();
-    document.getElementById("btn").remove();
-    loadScore();
-  }
-}
-
-function checkAns() {
-  const selectedAns = document.querySelector(
-    'input[name="answer"]:checked'
-  ).value;
-
-  if (selectedAns === Questions[currQuestion].correct_answer) {
-    score++;
-    nextQuestion();
-  } else {
-    nextQuestion();
-  }
-}
